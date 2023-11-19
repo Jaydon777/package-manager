16301ba0f8f6a7f7833b8514641eeb53570347b6
Jaddu <hanieljaydonj@gmail.com>
2023-11-19 22:20:10 +0530

    Revert "Update"
    This reverts commit 8f7b3ce4585e120c9b2d6ff847e9788d40bc9e1b.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import threading
 import logging
 from gi import require_version
 require_version('Gtk', '3.0')
-require_version('WebKit2', '4.0')
+require_version('WebKit2', '4.0')  # Add this line to require WebKit2 version
 from gi.repository import Gtk
 import subprocess
 import os
@@ -27,8 +27,11 @@ def is_package_installed_pip(package_name):
         return False
 
 def install_python_37():
+    # Add the deadsnakes PPA
     subprocess.run(['sudo', 'add-apt-repository', 'ppa:deadsnakes/ppa'])
+    # Update package information
     subprocess.run(['sudo', 'apt-get', 'update'])
+    # Install Python 3.7
     subprocess.run(['sudo', 'apt-get', 'install', 'python3.7'])
 
 def install_starting_packages():
@@ -40,7 +43,8 @@ def install_starting_packages():
     # Create a virtual environment named 'venv'
     subprocess.run(['python3.7', '-m', 'venv', 'venv'])
 
-    script_dir = os.path.dirname(os.path.abspath(_file_))
+    # Assuming the script is in the same folder as the venv folder
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     venv_path = os.path.join(script_dir, 'venv')
 
     # Modify the PATH variable to include the virtual environment's bin directory
@@ -49,7 +53,7 @@ def install_starting_packages():
     # Check if Flask is installed
     if not is_package_installed_pip('Flask'):
         subprocess.call(['python3', '-m', 'pip', 'install', 'flask[async]'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # subprocess.call(['python3', '-m', 'pip', 'install', 'flask[async]'])
+        #subprocess.call(['python3', '-m', 'pip', 'install', 'flask[async]'])
         logging.info("Installed Flask.")
     else:
         logging.info("Flask is already installed.")
@@ -57,25 +61,24 @@ def install_starting_packages():
     # Check if gir1.2-webkit2-4.0 is installed
     if not is_package_installed('gir1.2-webkit2-4.0'):
         subprocess.call(['sudo', 'apt-get', 'install', 'gir1.2-webkit2-4.0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # subprocess.call(['sudo', 'apt-get', 'install', 'gir1.2-webkit2-4.0'])
+        #subprocess.call(['sudo', 'apt-get', 'install', 'gir1.2-webkit2-4.0'])
         logging.info("Installed gir1.2-webkit2-4.0.")
     else:
         logging.info("gir1.2-webkit2-4.0 is already installed.")
 
 def run_flask_app():
-    # Set the FLASK_ENV environment variable to "production"
-    # os.environ['FLASK_ENV'] = 'production'
 
     # Run Flask in a separate process, capture stdout and stderr
     result = subprocess.run(['python3', 'flask_app/app.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    # result = subprocess.run(['python3', 'flask_app/app.py'])
+    #result = subprocess.run(['python3', 'flask_app/app.py'])
     
     # Log the stdout and stderr
     logging.info(f"Flask application stdout:\n{result.stdout}")
     logging.error(f"Flask application stderr:\n{result.stderr}")
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
+    # Install Flask and related packages if not already installed
     install_starting_packages()
 
     # Run Flask in a separate thread
@@ -83,8 +86,9 @@ if _name_ == "_main_":
     flask_thread.start()
     logging.info("Flask execution started!")
 
+
     # Create the WebViewWindow
     win = WebViewWindow()
 
     # Run the Gtk main loop
-    win.run()
+    win.run()
